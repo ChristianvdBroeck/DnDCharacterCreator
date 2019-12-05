c562752b8b4cbf8aaf9ff6153c720e1346920d39
Christian van den Broeck <c.v.d.broeck@wis.nl>
2019-12-05 17:07:12 +0100

    start of pdfWriter

---
+++ src/com/company/Controllers/PdfWriter.java
@@ -0,0 +1,20 @@
+package com.company.Controllers;
+
+/*
+ * Created by Christian van den Broeck on 05/12/2019
+ * ===
+ * THE PDF WRITER
+ *
+ */
+
+
+public class PdfWriter {
+
+    /*
+     * Calls from the front-end should come here.
+     * This controller should organise all properties and values passed with the call,
+     * and send them to the appropriate classes that (--fill in the sheet--)
+     *
+     * */
+
+}
